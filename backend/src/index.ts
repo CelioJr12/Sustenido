d66f74a17c22baa384662bc97b6c365dd714a745
CelioJr12 <celiojunio878@gmail.com>
2026-05-10 20:39:39 -0300

    uma parte do back implementada

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import streamsRouter from './routes/streams'
 
 dotenv.config()
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/streams', streamsRouter)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Sustenido API rodando!' })
